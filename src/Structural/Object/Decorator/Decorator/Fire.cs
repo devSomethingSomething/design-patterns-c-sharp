c746d4411b5d83425b765d81cb60bc942975ccf9
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-09-12 13:49:13 +0200

    Add two concrete weapon modifiers

---
+++ src/Structural/Object/Decorator/Decorator/Fire.cs
@@ -0,0 +1,22 @@
+﻿namespace Decorator
+{
+    public class Fire : WeaponModifierDecorator
+    {
+        private Weapon weapon;
+
+        public Fire(Weapon weapon)
+        {
+            this.weapon = weapon;
+        }
+
+        public override string GetDescription()
+        {
+            return weapon.GetDescription() + "\nFire";
+        }
+
+        public override int Damage()
+        {
+            return weapon.Damage() + 3;
+        }
+    }
+}
---
+++ src/Structural/Object/Decorator/Decorator/Heavy.cs
@@ -0,0 +1,22 @@
+﻿namespace Decorator
+{
+    public class Heavy : WeaponModifierDecorator
+    {
+        private Weapon weapon;
+
+        public Heavy(Weapon weapon)
+        {
+            this.weapon = weapon;
+        }
+
+        public override string GetDescription()
+        {
+            return weapon.GetDescription() + "\nHeavy";
+        }
+
+        public override int Damage()
+        {
+            return weapon.Damage() + 5;
+        }
+    }
+}
