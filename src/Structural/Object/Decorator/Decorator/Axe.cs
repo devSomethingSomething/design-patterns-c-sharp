e93602c68d84da628f62738988c3ff2ac0b8eed2
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-09-12 13:36:07 +0200

    Add two concrete weapon types

---
+++ src/Structural/Object/Decorator/Decorator/Axe.cs
@@ -0,0 +1,15 @@
+﻿namespace Decorator
+{
+    public class Axe : Weapon
+    {
+        public Axe()
+        {
+            Description = "Axe";
+        }
+
+        public override int Damage()
+        {
+            return 10;
+        }
+    }
+}
---
+++ src/Structural/Object/Decorator/Decorator/Sword.cs
@@ -0,0 +1,15 @@
+﻿namespace Decorator
+{
+    public class Sword : Weapon
+    {
+        public Sword()
+        {
+            Description = "Sword";
+        }
+
+        public override int Damage()
+        {
+            return 5;
+        }
+    }
+}
