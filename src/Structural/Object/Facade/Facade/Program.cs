578dbcf6d86fad746a6a01ba3324dc3771a21650
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-10-24 11:31:50 +0200

    Update Program.cs

--- src/Structural/Object/Facade/Facade/Program.cs
+++ src/Structural/Object/Facade/Facade/Program.cs
@@ -1,15 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Facade
+﻿namespace Facade
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            new RoomFacade(
+                new Light(),
+                new Light(),
+                new PC(),
+                new Speakers()).Setup();
+        }
+    }
+
+    public class Light
+    {
+        public void On() { }
+
+        public void Off() { }
+    }
+
+    public class PC
+    {
+        public void On() { }
+
+        public void Off() { }
+    }
+
+    public class Speakers
+    {
+        public void On() { }
+
+        public void Off() { }
+
+        public void SetVolume(int newVolume) { }
+
+        public void Mute() { }
+    }
+
+    public class RoomFacade
+    {
+        private Light deskLight;
+        private Light roomLight;
+
+        private PC pc;
+
+        private Speakers speakers;
+
+        public RoomFacade(Light deskLight, Light roomLight, PC pc, Speakers speakers)
+        {
+            this.deskLight = deskLight;
+            this.roomLight = roomLight;
+
+            this.pc = pc;
+
+            this.speakers = speakers;
+        }
+
+        public void Setup()
+        {
+            deskLight.On();
+            roomLight.On();
+
+            pc.On();
+
+            speakers.On();
+            speakers.SetVolume(50);
         }
     }
 }
